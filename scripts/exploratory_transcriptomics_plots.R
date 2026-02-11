e24164002e2df70e11e1f0f8e36b4511099cceb5
maziya <maziya.ibrahim@gmail.com>
2026-02-11 15:08:19 +0530

    more pca plots

--- scripts/exploratory_transcriptomics_plots.R
+++ scripts/exploratory_transcriptomics_plots.R
@@ -149,7 +149,7 @@ group <- group[sampleLabels]
 group <- factor(group)
 
 
-pca.res <- prcomp(t(log2.cpm.norm), scale. = FALSE, retx = TRUE)
+pca.res <- prcomp(t(log2.cpm.norm), scale. = TRUE, retx = TRUE)
 pc.var <- pca.res$sdev^2
 pc.per <- round(pc.var / sum(pc.var) * 100, 1)
 pca.res.df <- as_tibble(pca.res$x) %>%
@@ -179,7 +179,7 @@ v2_cols <- grep("V2$", colnames(QIV1_all.num), value = TRUE)
 
 QIV1.num.V1 <- QIV1_all.num[, v1_cols]
 QIV1.num.V2 <- QIV1_all.num[, v2_cols]
-myDGEList <- DGEList(QIV1.num.V1)
+myDGEList <- DGEList(QIV1_adjusted)
 myDGEList.norm <- calcNormFactors(myDGEList, method = "TMM")
 log2.cpm.norm <- cpm(myDGEList.norm, log=TRUE)
 
@@ -188,7 +188,7 @@ library(tidyverse)
 
 
 meta_long <- QIV1_meta_filt %>%
-  dplyr::select(SubjectIDNew, SEX, Library_Batch, starts_with("Responder_")) %>%
+  dplyr::select(SubjectIDNew, Visit, SEX, Library_Batch, starts_with("Responder_")) %>%
   tidyr::pivot_longer(cols = starts_with("Responder_"),
                       names_to = "Strain",
                       values_to = "ResponderStatus") %>%
@@ -198,50 +198,57 @@ samples_to_label <- c("QIV1KEM033V1", "QIV1KEM005V1", "QIV1KEM029V1","QIV1KEM084
 samples_to_label <- c("QIV1KEM033V2", "QIV1KEM005V2", "QIV1KEM029V2","QIV1KEM084V2","QIV1KEM094V2")
 
 
-sampleLabels <- colnames(log2.cpm.top50)
-pca.res <- prcomp(t(log2.cpm.top50), scale. = FALSE, retx = TRUE)
+sampleLabels <- colnames(log2.cpm.norm)
+pca.res <- prcomp(t(log2.cpm.norm), scale. = TRUE, retx = TRUE)
 pc.var <- pca.res$sdev^2
 pc.per <- round(pc.var / sum(pc.var) * 100, 1)
 
 pca.res.df <- as_tibble(pca.res$x) %>%
-  mutate(SubjectIDNew = sampleLabels)
+  dplyr::mutate(SubjectIDNew = sampleLabels)
 
 
 pca_long <- pca.res.df %>%
   left_join(meta_long, by = "SubjectIDNew") %>%
-  mutate(ResponderStatus = factor(ResponderStatus,
-                                  levels = c("Responder", "NonResponder")))
+  dplyr::mutate(ResponderStatus = factor(ResponderStatus,
+                                  levels = c("HR", "LR", "MR")))
+
+pca_long <- pca.res.df %>%
+  left_join(meta_long, by = "SubjectIDNew") %>%
+  # mutate(agegroup = factor(agegroup,
+  #                                 levels = c("60-70s", "70-80s")))  %>%
+  mutate(condition = if_else(is.na(condition), "nocondition", condition))
+
+
 library(ggrepel)
 
-pca.plot <- ggplot(pca_long, aes(x = PC1, y = PC2, color = ResponderStatus)) +
-  geom_point(size = 2) +
-  stat_ellipse(aes(group = ResponderStatus), type = "norm") +
-  geom_text_repel(
-    data = pca_long %>% dplyr::filter(SubjectIDNew %in% samples_to_label),
-    aes(label = SubjectIDNew),
-    size = 3,
-    max.overlaps = Inf
-  ) +
-  facet_wrap(~ Strain) +
+pca.plot <- ggplot(pca_long, aes(x = PC1, y = PC2, color = Visit)) +
+  geom_point(size = 1) +
+  stat_ellipse(aes(group = Visit), type = "norm") +
+  # geom_text_repel(
+  #   data = pca_long %>% dplyr::filter(SubjectIDNew %in% samples_to_label),
+  #   aes(label = SubjectIDNew),
+  #   size = 3,
+  #   max.overlaps = Inf
+  # ) +
+  # facet_wrap(~ Strain) +
   xlab(paste0("PC1 (", pc.per[1], "%)")) +
   ylab(paste0("PC2 (", pc.per[2], "%)")) +
-  labs(title = "PCA: Responder Status per Strain V1",
-       subtitle = "Top 50 genes(std.dev)log2 CPM, TMM normalized") +
+  labs(title = "PCA",
+       subtitle = "88 samples all genes logcpm") +
   coord_fixed() +
   theme_bw()
 
-ggsave("top50genesPCA_by_strain_responder_statusV1.png", plot = pca.plot, width = 16, height = 10)
+ggsave("PCA_visit_88samples_allgenes.png", plot = pca.plot, width = 16, height = 10)
 
 
 #standard deviation for each gene
 gene_sd = apply(log2.cpm.norm, 1, sd)
-top50_genes = names(sort(gene_sd, decreasing = TRUE))[1:50]
-top50 = sort(gene_sd, decreasing = TRUE)
+top5000_genes = names(sort(gene_sd, decreasing = TRUE))[1:5000]
+top5000 = sort(gene_sd, decreasing = TRUE)
 
 #expr matrix of the top50
-log2.cpm.top50 = log2.cpm.norm[top50_genes, ]
-
-
+log2.cpm.top5000 = log2.cpm.norm[top5000_genes, ]
+log2.cpm.lm22 = log2.cpm.norm[rownames(LM22_ensemblid),]
 ##################################################################
 ##################################################################
 library(stringr)
@@ -254,22 +261,25 @@ meta_long <- QIV1_meta_filt %>%
   tidyr::pivot_longer(cols = starts_with("Responder_"),
                       names_to = "Strain",
                       values_to = "ResponderStatus") %>%
-  mutate(Strain = str_remove(Strain, "^Responder_"))
+  dplyr::mutate(Strain = str_remove(Strain, "^Responder_"))
 
 sampleLabels <- colnames(log2.cpm.norm)
-pca.res <- prcomp(t(log2.cpm.norm), scale. = FALSE, retx = TRUE)
+pca.res <- prcomp(t(log2.cpm.norm), scale. = TRUE, retx = TRUE)
 pc.var <- pca.res$sdev^2
 pc.per <- round(pc.var / sum(pc.var) * 100, 1)
 
+
+
 pca.res.df <- as_tibble(pca.res$x) %>%
-  mutate(SubjectIDNew = sampleLabels)
+  dplyr::mutate(SubjectIDNew = sampleLabels)
 
 
 pca_long <- pca.res.df %>%
-  left_join(meta_long, by = "SubjectIDNew") %>%
-  mutate(ResponderStatus = factor(ResponderStatus,
-                                  levels = c("Responder", "NonResponder")),
+  dplyr::left_join(meta_long, by = "SubjectIDNew") %>%
+  dplyr::mutate(ResponderStatus = factor(ResponderStatus,
+                                  levels = c("LR", "MR","HR")),
          SEX = factor(SEX, levels = c("M","F")),
+         # AGE = AGE,
          Library_Batch = factor(Library_Batch))
 library(ggrepel)
 
@@ -284,9 +294,108 @@ pca.plot = ggplot(pca_long, aes(x = PC1, y = PC2, color = Library_Batch)) +
   coord_fixed() +
   theme_bw()
 
-ggsave("genesPCA_CombatSeq_batch.png", plot = pca.plot, width = 16, height = 10)
+ggsave("genesPCA_afterCombatSeq_batch_redo2.png", plot = pca.plot, width = 16, height = 10)
 
 pca_df = pca_long %>%
   mutate(IsOutlier = ifelse(abs(PC1) > 200 | abs(PC2) > 100, TRUE, FALSE))
 
 #sample QIV1KEM043V1 also has low gene counts
+
+
+pca_features <- pca.res$x[, 1:50]
+
+n_tsne_components <- 2
+tsne_result <- Rtsne(pca_features, dims = n_tsne_components, perplexity = 30, verbose = TRUE)
+tsne_df <- data.frame(X = tsne_result$Y[,1], Y = tsne_result$Y[,2])
+tsne_df$Label <- meta_long_str$ResponderStatus
+
+
+tSNE <- ggplot(tsne_df, aes(x = X, y = Y, color = Label)) +
+  geom_point(alpha = 0.7) +
+  theme_minimal() +
+  labs(
+    title = "Washington",
+    x = "t-SNE1", 
+    y = "t-SNE2"  
+  ) +
+  scale_color_manual(values = c("LR" = "brown", "HR" = "blue", "MR" = "orange")) +  
+  theme(legend.title = element_blank()) 
+
+ggsave(plot = tSNE,        
+       device = "png",          
+       filename = "tSNE_WH.png",
+       width = 300,
+       height = 300,
+       units = "px",
+       dpi = 300,
+       scale = 6)
+
+
+
+
+QIV1_meta_filt <- QIV1_meta_filt %>%
+      mutate(agegroup = case_when(
+            AGE >= 60 & AGE < 70 ~ "60-70s",
+            AGE >= 70 ~ "70-80s"))
+
+age_v1 <- QIV1_meta_filt %>%
+     filter(Visit == "V1") %>%
+    pull(agegroup)
+
+
+### Checking correlation of PC components with the covariates
+pca_scores = pca.res.df[,1:25]
+meta = QIV1_meta_filt[,2:5]
+rownames(meta) = rownames(QIV1_meta_filt)
+
+test_association <- function(pc, covariate) {
+  if (is.numeric(covariate)) {
+    #continuous (like AGE), 
+    pval <- cor.test(pc, covariate)$p.value
+  } else {
+    #categorical (like Batch or SEX)
+    model <- aov(pc ~ as.factor(covariate))
+    pval <- summary(model)[[1]][["Pr(>F)"]][1]
+  }
+  return(pval)
+}
+
+results_list <- list()
+
+for (pc_name in colnames(pca_scores)) {
+  for (meta_name in colnames(meta)) {
+    
+    p_val <- test_association(pca_scores[[pc_name]], meta[[meta_name]])
+    results_list[[length(results_list) + 1]] <- data.frame(
+      PC = pc_name,
+      Variable = meta_name,
+      P_Value = p_val,
+      negLog10PVal = -log10(p_val) 
+    )
+  }
+}
+
+correlation_results <- bind_rows(results_list)
+correlation_results$PC <- factor(
+  correlation_results$PC,
+  levels = correlation_results$PC[order(correlation_results$PC_num)] |> unique()
+)
+correplation_covariate = ggplot(correlation_results, aes(x =PC, y = Variable, size = negLog10PVal)) +
+  geom_point(color = "darkgrey") + theme_minimal()+
+  labs(size = "-log10(pvalue)", x = "", y = "", title = "Correlation of covariate with PC component after combatseq")
+ggsave("pc_corr_covariate.png", plot = correplation_covariate, width = 12)
+
+
+
+
+scree_df <- data.frame(
+  PC = paste0("PC", 1:25),
+  VariancePercent = pc.per[1:25]
+)
+screepc = ggplot(scree_df, aes(x = factor(PC, levels = PC), 
+                     y = VariancePercent)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Principal Component",
+       y = "Variance Explained (%)") +
+  theme(axis.text.x = element_text(angle = 45))
+ggsave("pc_screeplot.png", plot = screepc, width = 10)
