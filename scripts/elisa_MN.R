6e4be7c015e2c9fabc20ebc50acc053f454728f6
maziya <maziya.ibrahim@gmail.com>
2026-05-13 16:26:48 +0530

    update

--- scripts/elisa_MN.R
+++ scripts/elisa_MN.R
@@ -108,7 +108,8 @@ plot2 = ggplot(QIV1_MN_long,
   geom_line(alpha = 0.15) +
   geom_point(alpha = 0.4) +
   labs(title    = "62 Subjects data from all 3 assays",
-       subtitle = "Aggregate ResponderGroup used here")+
+       subtitle = "Aggregate ResponderGroup used here",
+       y = "Max(Foldchange (D28/D0))")+
   stat_summary(aes(group = ResponderGroup),
                fun = median,
                geom = "line",
@@ -123,3 +124,216 @@ plot2 = ggplot(QIV1_MN_long,
   theme_bw()
 ggsave("subject_3assay_response.png", plot = plot2,
        width = 12, height = 8, dpi = 600, bg = "white")
+
+
+plot3 <- ggplot(QIV1_MN_long,
+                aes(Assay, MaxFold, group = SubjectID)) +
+  
+  # subject trajectories
+  geom_line(alpha = 0.08,
+            linewidth = 0.4,
+            color = "grey40") +
+  
+  # individual points
+  geom_point(
+    alpha = 0.7,
+    size = 1,
+    position = position_jitter(width = 0.04)) +
+  
+  # overall median trend
+  stat_summary(aes(group = 1),
+               fun = median,
+               geom = "line",
+               linewidth = 1.8,
+               color = "#A020F0") +
+  
+  stat_summary(aes(group = 1),
+               fun = median,
+               geom = "point",
+               size =3,
+               color = "#A020F0") +
+  
+  scale_y_log10() +
+  
+  labs(
+    title = "Max Fold Change Across Assays for 62 Subjects",
+    subtitle = "Individual subject trajectories with overall median trend",
+    x = NULL,
+    y = "Max Fold Change (log10 scale)") +
+  
+  theme_bw(base_size = 14) +
+  theme(
+    panel.grid.minor = element_blank(),
+    panel.grid.major.x = element_blank(),
+    plot.title = element_text(face = "bold"),
+    legend.position = "right",
+    panel.border = element_rect(linewidth = 0.8))
+
+ggsave("subject_3assay_response_v4.png",
+       plot = plot3,
+       width = 9,
+       height = 6,
+       dpi = 600,
+       bg = "white")
+
+
+
+hk <- QIV1_HAI_MN %>%
+  select(
+    SubjectID,
+    BL.A.HongKong,
+    PostVac.A.HongKong,
+    D0_HongKong,
+    D28_HongKong
+  ) %>%
+  rename(
+    HAI_BL = BL.A.HongKong,
+    HAI_PostVac = PostVac.A.HongKong,
+    MN_D0 = D0_HongKong,
+    MN_D28 = D28_HongKong
+  ) %>%
+  mutate(Strain = "HongKong")
+
+
+vc <- QIV1_HAI_MN %>%
+  select(
+    SubjectID,
+    BL.A.Victoria,
+    PostVac.A.Victoria,
+    D0_Victoria,
+    D28_Victoria
+  ) %>%
+  rename(
+    HAI_BL = BL.A.Victoria,
+    HAI_PostVac = PostVac.A.Victoria,
+    MN_D0 = D0_Victoria,
+    MN_D28 = D28_Victoria
+  ) %>%
+  mutate(Strain = "Victoria")
+
+
+ph <- QIV1_HAI_MN %>%
+  select(
+    SubjectID,
+    BL.B.Phuket,
+    PostVac.B.Phuket,
+    D0_Phuket,
+    D28_Phuket
+  ) %>%
+  rename(
+    HAI_BL = BL.B.Phuket,
+    HAI_PostVac = PostVac.B.Phuket,
+    MN_D0 = D0_Phuket,
+    MN_D28 = D28_Phuket
+  ) %>%
+  mutate(Strain = "Phuket")
+
+wh <- QIV1_HAI_MN %>%
+  select(
+    SubjectID,
+    BL.B.Washington,
+    PostVac.B.Washington,
+    D0_Washington,
+    D28_Washington
+  ) %>%
+  rename(
+    HAI_BL = BL.B.Washington,
+    HAI_PostVac = PostVac.B.Washington,
+    MN_D0 = D0_Washington,
+    MN_D28 = D28_Washington
+  ) %>%
+  mutate(Strain = "Washington")
+
+
+combined <- bind_rows(hk, vc, ph, wh)
+
+plot_df <- combined %>%
+  pivot_longer(
+    cols = -c(SubjectID, Strain),
+    names_to = c("Assay", "Timepoint"),
+    names_sep = "_",
+    values_to = "Value"
+  ) %>%
+  mutate(
+    Timepoint = factor(
+      Timepoint,
+      levels = c("BL", "D0", "PostVac", "D28")
+    )
+  )
+plot2 = ggplot(plot_df,
+               aes(Timepoint,
+                   Value,
+                   group = interaction(SubjectID, Assay),
+                   color = Assay)) +
+  
+  geom_line(alpha = 0.2) +
+  geom_point(size = 1.5, alpha = 0.7) +
+  
+  facet_wrap(~Strain) +
+  
+  scale_y_log10() +
+  
+  theme_bw(base_size = 14) +
+  
+  labs(
+    title = "MN and HAI responses across strains",
+    x = NULL,
+    y = "Response / Titer (log10 scale)"
+  ) +
+  
+  theme(
+    plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
+    panel.grid.minor = element_blank(),
+    strip.background = element_rect(fill = "white"),
+  )
+ggsave("subject_HAI_MN_response.png",
+       plot = plot2,
+       width = 9,
+       height = 6,
+       dpi = 600,
+       bg = "white")
+
+plot_df <- plot_df %>%
+  mutate(
+    Stage = case_when(
+      Timepoint %in% c("BL", "D0") ~ "Baseline",
+      Timepoint %in% c("PostVac", "D28") ~ "PostVaccination"
+    )
+  )
+
+
+plot3 = ggplot(plot_df,
+               aes(x = Assay,
+                   y = Value,
+                   group = interaction(SubjectID, Stage),
+                   color = Stage)) +
+  
+  geom_line(alpha = 0.25,
+            linewidth = 0.5) +
+  
+  geom_point(size = 2,
+             alpha = 0.8) +
+  
+  facet_wrap(~Strain) +
+  
+  scale_y_log10() +
+  
+  theme_bw(base_size = 14) +
+  
+  labs(
+    title = "HAI and MN ",
+    x = NULL,
+    y = "Response / Titer (log10 scale)"
+  ) +
+  
+  theme(
+    plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
+    panel.grid.minor = element_blank(),
+    strip.background = element_rect(fill = "white"),
+  )
+ggsave("subject_HAI_MN_response_v2.png",
+       plot = plot3,
+       width = 9,
+       height = 6,
+       dpi = 600,
+       bg = "white")
