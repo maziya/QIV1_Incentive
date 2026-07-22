0575f4e83960c597410da878104fc23fae2f133c
maziya <maziya.ibrahim@gmail.com>
2026-07-22 20:00:26 +0530

    NEMO to cluster samples

--- scripts/MOFA_4omics.R
+++ scripts/MOFA_4omics.R
@@ -1,26 +1,79 @@
 library(MOFA2)
 
-transcriptomics <- transcriptomics %>%
+
+transcriptomics_V2 <- transcriptomics[, endsWith(colnames(transcriptomics), "V2")]
+
+Olink_V2 <- Olink %>%
+            select(1, ends_with("V2"))
+proteomics_V2 <- proteomics %>%
+                select(1, ends_with("V2"))
+metabolomics_V2 <- metabolomics %>%
+  select(1, ends_with("V2"))
+
+
+
+transcriptomics_V2 <- transcriptomics_V2 %>%
   tibble::rownames_to_column("Gene")
-transcriptomics_long = transcriptomics %>%
+transcriptomics_long = transcriptomics_V2 %>%
   pivot_longer(cols = -Gene,
                names_to = "SubjectID",
                values_to = "Expression")
+
+ssgsea_scores_fil = ssgsea_V2_BTMscores #taken from csv file of BTM scores V2
+btm_variance <- apply(ssgsea_scores_fil, 1, var)
+variance_threshold <- quantile(btm_variance, 0.4)
+ssgsea_scores_fil <- ssgsea_scores_fil[btm_variance > variance_threshold, ]
+
+btm_fil = as.data.frame(ssgsea_scores_fil)
+btm_fil = rownames_to_column(btm_fil, var= "BTM")
+BTM_long = btm_fil %>%
+  pivot_longer(cols = -BTM,
+               names_to = "sample",
+               values_to = "value") %>%
+  mutate(view = "BTM")
+
+#WGCNA_V1 and WGCNA_V2 come from net_V1[["MEs"]] of wgcna runs
+wgcna = t(WGCNA_V2)
+wgcna = as.data.frame(wgcna)
+wgcna_fil = rownames_to_column(wgcna, var= "Modules")
+wgcna_long = wgcna_fil %>%
+  pivot_longer(cols = -Modules,
+               names_to = "sample",
+               values_to = "value") %>%
+  mutate(view = "WGCNA")
+
 metabolomics = as.data.frame(metabolomics)
 metabolomics <- metabolomics %>%
   tibble::rownames_to_column("Metabolite")
 
-metabolomics_long = metabolomics %>%
+metabolomics_long = metabolomics_V2 %>%
   pivot_longer(cols = -Metabolite,
                names_to = "SubjectID",
                values_to = "Expression")
 
+proteomics_long = proteomics_V2 %>%
+  pivot_longer(cols = -Protein,
+               names_to = "SubjectID",
+               values_to = "Expression")
+
+Olink_long = Olink_V2 %>%
+  pivot_longer(cols = -Protein,
+               names_to = "SubjectID",
+               values_to = "Expression")
+
 colnames(Olink_long)[1] = "Feature"
+colnames(metabolomics_long)[1] = "Feature"
+colnames(transcriptomics_long)[1] = "Feature"
+colnames(proteomics_long)[1] = "Feature"
+
 
 transcriptomics_long<- transcriptomics_long %>%
 rename(sample = SubjectID,
-value = expression) %>%
+value = Expression) %>%
 mutate(view = "transcriptomics")
+
+transcriptomics_long = transcriptomics_long %>% filter(sample %in% QIV1_meta_filt_cond_V2$SubjectIDNew)
+
 
 proteomics_long<- proteomics_long %>%
   rename(sample = SubjectID,
@@ -28,25 +81,29 @@ proteomics_long<- proteomics_long %>%
   mutate(view = "proteomics")
 
 proteomics_long <- proteomics_long %>%
-  filter(sample %in% transcriptomics_long$sample)
+  filter(sample %in% BTM_long$sample)
 
 Olink_long<- Olink_long %>%
   rename(sample = SubjectID,
-         value = Expression) %>%
+         value = Expression)%>%
   mutate(view = "olink")
 
 Olink_long <- Olink_long %>%
-  filter(sample %in% transcriptomics_long$sample)
+  filter(sample %in% BTM_long$sample)
 
 metabolomics_long<- metabolomics_long %>%
   rename(sample = SubjectID,
          value = Expression) %>%
-  mutate(view = "metabolomics")
+  mutate(view = "metabolomics") 
 
 metabolomics_long <- metabolomics_long %>%
-  filter(sample %in% transcriptomics_long$sample)
+  filter(sample %in% BTM_long$sample)
 
-data = bind_rows(transcriptomics_long,proteomics_long,Olink_long,metabolomics_long)
+colnames(BTM_long)[1] = "Feature"
+colnames(wgcna_long)[1] = "Feature"
+data = bind_rows(proteomics_long,Olink_long,metabolomics_long)
+data = bind_rows(wgcna_long,Olink_long)
+
 colnames(data)[1] ="feature"
 data= data[, c("sample", "feature", "view","value")]
 
@@ -72,6 +129,7 @@ for(v in views){
 data_opts$scale_views = TRUE 
 
 model_opts = get_default_model_options(MOFAobject)
+model_opts$num_factors <- 7
 print(model_opts)
 train_opts = get_default_training_options(MOFAobject)
 MOFAobject = prepare_mofa(
@@ -82,8 +140,49 @@ MOFAobject = prepare_mofa(
 )
 
 
-outfile = file.path(getwd(),"QIVIMOFA_model.hdf5")
-MOFAobject.trained <- run_mofa(MOFAobject, outfile, use_basilisk=TRUE)
+outfile = file.path(getwd(),"QIVI_V1_olinkuntargmetsMOFA.hdf5")
+MOFAobject.trained = run_mofa(MOFAobject, outfile, use_basilisk=TRUE)
+
+
+var_exp <- get_variance_explained(MOFAobject.trained)
+
+
+df3 <- as.data.frame(var_exp$r2_per_factor[[1]])
+
+df3$Factor <- rownames(df3)
+
+df3_long <- df3 %>%
+  pivot_longer(
+    cols = -Factor,
+    names_to = "View",
+    values_to = "VarianceExplained"
+  )
+df3_long <- reshape2::melt(var_exp$r2_per_factor[[1]])
+
+df3_long = df3_long %>%
+  mutate(
+    View = case_when(
+      View == "BTM" ~ "BloodTranscriptionalModule",
+      TRUE ~ View))
+colnames(df3_long) <- c("Factor", "View", "VarianceExplained")
+
+df3_long = df3_long %>% filter(Factor %in% c("Factor1", "Factor2","Factor3"))
+factor_varplot = ggplot(df3_long,
+                        aes(Factor,
+                            VarianceExplained,
+                            fill = View)) +
+  
+  geom_col(position = "dodge") +
+  labs(title = "Percentage variance explained by each view")+
+  coord_flip() +
+  
+  theme_classic()+
+                theme(plot.title = element_text(size = 16),
+                legend.title = element_text(size = 12),
+                legend.text = element_text(size = 10))
+
+ggsave("factor_varplot.png", plot = factor_varplot, width = 6, height = 6, dpi = 600)
+
 v = plot_variance_explained(MOFAobject.trained)
 ggsave("totalvariancebyfactor_mofa.png", plot = v)
 print(MOFAobject.trained@cache$variance_explained$r2_per_factor)
@@ -95,62 +194,265 @@ print(MOFAobject.trained@cache$variance_explained$r2_total)
 
 
 #add metadata
-sample_metadata = data.frame(sample=QIV1_meta_filt$SubjectIDNew,
-                                           responder =QIV1_meta_filt$Responder_HongKong,visit = QIV1_meta_filt$Visit)
+sample_metadata = data.frame(sample=QIV1_meta_filt_cond1$SubjectIDNew,
+                                           responder =QIV1_meta_filt_cond1$ResponderGroup,visit = QIV1_meta_filt_cond1$Visit,
+                             age = QIV1_meta_filt_cond1$AGE, sex =QIV1_meta_filt_cond1$SEX)
+
+sample_metadata = sample_metadata %>% mutate(visit = case_when(visit == "V2" ~ "Day3", visit == "V1" ~ "Day0" )) 
+sample_metadata = sample_metadata %>% filter(visit == "Day0")
+
+
 samples_metadata(MOFAobject.trained) = sample_metadata
 
-p1= plot_factor(MOFAobject.trained, 
-            factor = 4:5,
-            color_by = "responder",
-            dot_size = 3,
-            dodge = T,
-            legend = T,
-            add_violin = T,
-            violin_alpha = 0.25)
-library(ggplot2)
-p1 = p1 + scale_color_manual(values = c("MR"="black", "LR"="blue", "HR"="purple"))+
-  scale_fill_manual(values = c("MR"="black", "LR"="blue","HR"="purple"))
-print(p)
+# p1= plot_factor(MOFAobject.trained, 
+#             factor = 3,
+#             color_by = "responder",
+#             dot_size = 3,
+#             dodge = T,
+#             legend = T,
+#             add_violin = T,
+#             violin_alpha = 0.25) 
 
-ggsave("factor4&5_olink&transcriptomics_visit.png",plot = p)
+meta <- samples_metadata(MOFAobject.trained)
+
+df <- cbind(meta, factors)
+
+comparisons <- list(
+  c("LR","MR"),
+  c("MR","HR"),
+  c("LR","HR")
+)
+df_long <- pivot_longer(
+  df,
+  cols = c(Factor1, Factor2,Factor4),
+  names_to = "Factor",
+  values_to = "Value"
+)
+df2_long <- pivot_longer(
+  df,
+  cols = c(Factor2),
+  names_to = "Factor",
+  values_to = "Value"
+)
+
+df2_long$responder <- factor(df2_long$responder, levels = c("LR", "MR","HR"))
+
+p <- ggplot(df2_long, aes(x = responder, y = Value, fill = responder)) +
+  geom_boxplot(alpha = 0.3, notch = FALSE) +
+  geom_jitter(width = 0.1) +
+  stat_compare_means(
+    comparisons = comparisons,
+    method = "wilcox.test",
+    label = "p.format",
+    p.adjust.method = "BH",
+    size = 4,
+    step.increase = 0.12
+  ) +
+  # facet_wrap(~Factor, scales = "free_y") +
+  theme_classic() +
+  labs(title = "Day0",
+    x = "Responder Group",
+    y = "MOFA factor 2 value"
+  )+
+  theme_classic()
+
+ggsave("Factor2_aggregateres_v2.png", p, width = 6, height = 5, dpi = 300)
+
+
+library(ggplot2)
+
+
+s1 = plot_factor_cor(MOFAobject.trained)
+s2 = plot_factors_vs_cov(MOFAobject.trained)
 
 #The sign of the weights indicates the direction of the effect: a positive weights 
 #indicates that the feature has higher levels in the samples with positive factor 
 #values, and vice-versa.
 
-q = plot_weights(MOFAobject.trained,view = "olink", factors = 4, 
+
+
+q = plot_weights(MOFAobject.trained,view = "BTM", factors = 1, 
                  nfeatures = 10, scale = T, abs = F, text_size = 2.5)
 print(q)
-ggsave("factor4_olink_weights.png",plot = q)
+ggsave("factor1_BTM_weights.png",plot = q)
 
-plot_top_weights(MOFAobject.trained,view = "transcriptomics",
-                 factor = 4,
+plot_top_weights(MOFAobject.trained,view = "BTM",
+                 factor = 2,
                  nfeatures = 10,    
                  scale = T)
 
 #which genes drive the factor based on weights and get an association of features
 #and factors in a scatterplot
 q1 = plot_data_scatter(MOFAobject.trained, 
-                  view = "transcriptomics", 
-                  factor = 1, 
+                  view = "olink", 
+                  factor = 2, 
                   features = 12,
                   color_by = "responder",
                   add_lm = T,
                   dot_size = 1)
-ggsave("factor1_transcriptomics_responder_weightsscatter1.png",plot = q1, width = 10)
+ggsave("factor2_olink_weightsscatter.png",plot = q1, width = 10, height = 10)
 
-plot_factor_cor(MOFAobject.trained) #check correlation of factors
+p3 = plot_factor_cor(MOFAobject.trained) #check correlation of factors
 #they should not be correlated that would mean it is a poor model fit
 
-ggsave("allfactors_Corr.png",plot = p3)
+p4 = correlate_factors_with_covariates(MOFAobject.trained, covariates = c('AGE','SEX'),
+                                  plot = "log_pval")
+ggsave("factors_corr_covariates.png",plot = p4)
 
 sample_group = samples_metadata(MOFAobject.trained)
 rownames(sample_group) = sample_group[,1]
 h1 = heatmap_plot = plot_data_heatmap(MOFAobject.trained, 
-                                  view = "transcriptomics",
-                                  factor = 1, features = 50,
+                                  view = "WGCNA",
+                                  factor = 1, features = 20,
                                   cluster_rows = TRUE, cluster_cols = TRUE,
                                   annotation_samples = sample_group[,"responder",drop=F],
                                   show_rownames = TRUE, show_colnames = FALSE,
                                   scale = "row")
-ggsave("factor1_transcriptomics_responder_heatmap.png",plot = h1, width = 10, height = 10)
+ggsave("factor1_WGCNA_responder_heatmap.png",plot = h1, width = 10, height = 10)
+
+factors <- MOFA2::get_factors(MOFAobject.trained, factors = "all")[[1]]
+clusters= cluster_samples(MOFAobject.trained,k=2,factors = 1)
+samples_metadata$Cluster = factor(clusters$cluster)
+
+df2 <- as.data.frame(factors)
+df2$Cluster <- factor(clusters$cluster)
+df2$visit = factor(sample_metadata$visit)
+df2$responder = factor(sample_metadata$responder)
+
+c1 = ggplot(df2, aes(x = Factor1, y = Factor2,
+                    color = responder, shape = Cluster)) +
+  geom_point(size = 3, alpha = 0.9) +
+  theme_classic(base_size = 12) +
+  labs(
+    title = "MOFA Sample Clusters (k = 2)",
+    x = "Factor 1",
+    y = "Factor 2",
+    shape = "Cluster",
+    color = "Responder"
+  ) +
+  theme(
+    legend.position = "right",
+    plot.title = element_text(face = "bold", hjust = 0.5)
+  )
+ggsave("factor12_clustersamples_mofa.png",plot = c1, width = 10, height = 10)
+
+
+set.seed(12345)
+MOFAobject_umap <- run_umap(MOFAobject.trained)
+MOFAobject_tsne <- run_tsne(MOFAobject.trained)
+
+p <- plot_dimred(
+  MOFAobject_umap,
+  method = "UMAP",
+  color_by = "Factor1",
+  dot_size = 3
+)
+
+ggsave("mofa_umap_factor1.png", p, width = 6, height = 5)
+category.colors <- c(
+  "HR" = "darkturquoise", 
+  "LR" = "magenta3", "MR" = "#FFCC99")
+plot_factors(MOFAobject.trained, 
+             factors = c(1,2), 
+             color_by = "responder", 
+             dot_size = 4) + 
+  scale_fill_manual(values=category.colors) +
+  stat_ellipse(aes(color=color_by), geom = "polygon", alpha=0.25) +
+  scale_color_manual(values=category.colors)
+
+
+covariates = QIV1_meta_filt_cond_V2 %>% dplyr::select(SubjectIDNew,SEX,AGE)
+covariates = column_to_rownames(covariates, var = "SubjectIDNew")
+covariates <- covariates %>%
+mutate(SEX = ifelse(SEX == "F", 1, 0))
+
+plot_variance_explained_per_feature(MOFAobject.trained,view="WGCNA",split_by_factor = TRUE, features = 10, factors = 2)
+
+
+library(dplyr)
+library(ggplot2)
+weights =MOFA2::get_weights(MOFAobject.trained, as.data.frame = TRUE)
+weights_factor1 = weights %>% filter(factor == "Factor2")
+
+weights_factor_olink = weights_factor1 %>% filter(view== "olink")
+colnames(weights_factor_olink)[1] = "uniprotid"
+weights_factor_olink = left_join(weights_factor_olink,olink_ENSG_HGNC, by = "uniprotid")
+weights_factor_olink = weights_factor_olink[,-c(1,5)]
+colnames(weights_factor_olink)[4] = "feature"
+weights_factor_olink= weights_factor_olink[, c("feature", "factor", "value","view")]
+weights_factor1 = weights_factor1[-c(45:135),]
+weights_factor1 = rbind(weights_factor1, weights_factor_olink)
+
+
+
+top_pos <- weights_factor1 %>%
+  group_by(view) %>%
+  arrange(desc(value), .by_group = TRUE) %>%
+  slice(1:10)
+
+top_neg <- weights_factor1 %>%
+  group_by(view) %>%
+  arrange(value, .by_group = TRUE) %>%
+  slice(1:10)
+
+top_features <- bind_rows(top_pos, top_neg)
+
+q3 <- ggplot(top_features,
+            aes(x = reorder(feature, value),
+                y = value,
+                fill = value > 0)) +
+  geom_bar(stat = "identity") +
+  coord_flip() +
+  facet_wrap(~view, scales = "free_y") +
+  scale_fill_manual(values = c("#6A3D9A","#FF7F00"), guide = "none") +
+  theme_classic() +
+  theme(text = element_text(family = "Arial"), axis.text.y = element_text(size = 16),   
+        axis.title.y = element_text(size = 16),  
+        axis.text.x = element_text(size = 14),
+        strip.text = element_text(size = 16)) +
+  labs(
+    x = "",
+    y = "MOFA weight",
+    title = "Top drivers of Factor 2 by view Day0"
+  )
+
+ggsave("Factor2_top_drivers_by_view_v1.png", plot = q3, width = 15, height = 10, dpi = 300)
+
+
+ssGSEA_V1_BTMscores <- read_csv("~/INCENTIVE/RNASeq/QIV1_DEG_Analysis/results/baseline_predictors/ssGSEA_V1_BTMscores.csv")
+colnames(ssGSEA_V1_BTMscores)[1] = "Feature"
+ssGSEA_V1_BTMscores = pivot_longer(ssGSEA_V1_BTMscores, cols = -Feature,names_to = "sample", values_to = "value" )
+colnames(ssGSEA_V1_BTMscores)[3] = "BTMscore"
+ssGSEA_V1_BTMscores = ssGSEA_V1_BTMscores %>% filter(Feature %in% BTM_long_sel$Feature)
+BTM_avg = ssGSEA_V1_BTMscores %>%
+  group_by(Feature) %>%
+  summarise(mean_BTMscore = mean(BTMscore))
+colnames(BTM_avg)[1]= "feature"
+BTM_avg  = left_join(BTM_avg,top_features)
+ssGSEA_V1_BTMscores = ssGSEA_V1_BTMscores %>% filter(Feature %in% top_pos4$feature[1:3])
+
+ssGSEA_V1_BTMscores = left_join(ssGSEA_V1_BTMscores,df2_long,by="sample")
+
+scat = ggplot(ssGSEA_V1_BTMscores, aes(x = Value.x, y = BTMscore)) +
+geom_point(size = 3) +
+theme_classic()+ stat_cor(method = "spearman") +
+labs(
+x = "MOFA factor4 value",
+y = "BTM scores"
+)
+ggsave("BTMvsfactor4_value_scatter.png", scat)
+
+
+ssGSEA_V1_BTMscores = left_join(ssGSEA_V1_BTMscores, sample_metadata, by = "sample")
+plot2 = ggplot(ssGSEA_V1_BTMscores, aes(x=responder, y= BTMscore, fill= responder)) + geom_boxplot(alpha = 0.3)+
+  facet_wrap(~Feature)+
+  stat_compare_means(
+    comparisons = comparisons,
+    method = "wilcox.test",
+    label = "p.format",
+    p.adjust.method = "BH",
+    size = 4,
+    step.increase = 0.12
+  ) +theme_bw()+
+  labs(title = "BTMscores of responder HK Day0", x= "responder", y="BTMscores")
+ggsave("topBTM_vsresponder_HK.png", plot2, width = 10)
---
+++ scripts/NEMO.R
@@ -0,0 +1,168 @@
+# k can also be set to NA, in which case nemo chooses its value.
+# nemo.affinity.graph is the integrated affinity graph.
+# 
+if (!requireNamespace("SNFtool", quietly = TRUE)) {
+install.packages("SNFtool")
+}
+library(SNFtool)
+devtools::install_github('Shamir-Lab/NEMO/NEMO')
+library(NEMO)
+
+BTM_wide = pivot_wider(BTM_long, names_from = sample, values_from = value)
+BTM_wide = BTM_wide[,-c(2)]
+BTM_wide = column_to_rownames(BTM_wide, var="Feature")
+btm_variance <- apply(BTM_wide, 1, var)
+variance_threshold <- quantile(btm_variance, 0.2)
+BTM_sel = BTM_wide[btm_variance > variance_threshold, ]
+
+proteomics_sel = proteomics_V1[,which(colnames(proteomics_V1)%in%colnames(BTM_sel))]
+rownames(proteomics_sel)= rownames(proteomics_V1)
+Olink_sel = Olink_V1[,which(colnames(Olink_V1)%in%colnames(BTM_sel))]
+rownames(Olink_sel) = Olink_V1$Protein
+metabolomics_sel = metabolomics_V1[,which(colnames(metabolomics_V1)%in%colnames(BTM_sel))]
+rownames(metabolomics_sel) = metabolomics_V1$Metabolite
+
+omics.list = list(BTM_sel,proteomics_sel,Olink_sel)
+
+
+set.seed(42)
+affinity.graph = nemo.affinity.graph(omics.list, k = 10)
+
+  # nemo to estimate the number of clusters
+num.clusters = nemo.num.clusters(affinity.graph)
+
+# clustering is the cluster assignment vector, ordered by the columns of affinity.graph.
+clustering = spectralClustering(affinity.graph, num.clusters)
+names(clustering) = colnames(affinity.graph)
+
+clust.df = as.data.frame(clustering)
+clust.df = rownames_to_column(clust.df,var="SubjectID")
+clust.df$SubjectID = gsub("V1$","", clust.df$SubjectID)
+clust.df = inner_join(clust.df,ResponderGroups, by = "SubjectID")
+clust.df = inner_join(clust.df,responder_groups, by = "SubjectID")
+
+clustering2 = spectralClustering(affinity.graph, 3)
+names(clustering2) = colnames(affinity.graph)
+clust.df2 = as.data.frame(clustering2)
+clust.df2 = rownames_to_column(clust.df2,var="SubjectID")
+clust.df2$SubjectID = gsub("V1$","", clust.df2$SubjectID)
+clust.df2 = inner_join(clust.df2,ResponderGroups, by = "SubjectID")
+clust.df2 = inner_join(clust.df2,responder_groups, by = "SubjectID")
+
+
+library(mclust) 
+
+# clustering: named vector from spectralClustering, e.g. clustering["P1"] = 1
+# labels:  HR/MR/LR labels, same patient order/names
+
+ari_HK <- adjustedRandIndex(clust.df2$clustering, clust.df2$HongKong)
+print(ari)
+set.seed(42)
+perm_aris <- replicate(1000, adjustedRandIndex(clust.df2$clustering, sample(clust.df2$HongKong)))
+p_value <- mean(perm_aris >= ari)
+
+adjustedRandIndex(clust.df2$clustering2,clust.df2$HongKong)
+adjustedRandIndex(clust.df2$clustering2, clust.df2$Victoria)
+adjustedRandIndex(clust.df2$clustering2, clust.df2$Phuket)
+adjustedRandIndex(clust.df2$clustering2, clust.df2$Washington)
+
+
+
+library(pheatmap)
+cont_table <- table(fused.clusters$clustering_k2, fused.clusters$HongKong)
+pheatmap(cont_table, cluster_rows = FALSE, cluster_cols = FALSE,
+         display_numbers = TRUE, main = "NEMO clusters vs Responder status")
+
+
+clustering_k2 <- spectralClustering(affinity.graph, 2)
+names(clustering_k2) <- colnames(affinity.graph)
+table(clustering_k2)
+names(clustering_k2) = colnames(affinity.graph)
+fused.clusters = as.data.frame(clustering_k2)
+fused.clusters = rownames_to_column(fused.clusters,var="SubjectID")
+fused.clusters$SubjectID = gsub("V1$","", fused.clusters$SubjectID)
+fused.clusters = inner_join(fused.clusters,ResponderGroups, by = "SubjectID")
+fused.clusters = inner_join(fused.clusters,responder_groups, by = "SubjectID")
+
+
+#clustering per omics layer
+per.omic.graphs <- lapply(names(omics.list), function(name) {
+  nemo.affinity.graph(list(omics.list[[name]]), k = 5)
+})
+names(per.omic.graphs) <- names(omics.list)
+
+num.clusters <- 3 
+set.seed(42) 
+per.omic.clusters <- lapply(per.omic.graphs, function(aff) {
+  spectralClustering(aff, K = num.clusters)
+})
+names(per.omic.clusters) <- names(omics.list)
+set.seed(42) 
+fused.aff <- nemo.affinity.graph(omics.list, k = 5)
+fused.clusters <- spectralClustering(fused.aff, K = num.clusters)
+
+
+
+names(fused.clusters) <- colnames(fused.aff)
+table(fused.clusters)
+names(fused.clusters) = colnames(fused.aff)
+fused.clusters.df = as.data.frame(fused.clusters)
+fused.clusters.df = rownames_to_column(fused.clusters.df,var="SubjectID")
+fused.clusters.df$SubjectID = gsub("V1$","", fused.clusters.df$SubjectID)
+fused.clusters.df = inner_join(fused.clusters.df,ResponderGroups, by = "SubjectID")
+fused.clusters.df = inner_join(fused.clusters.df,responder_groups, by = "SubjectID")
+
+adjustedRandIndex(fused.clusters.df$fused.clusters,fused.clusters.df$HongKong)
+adjustedRandIndex(fused.clusters.df$fused.clusters, fused.clusters.df$Victoria)
+adjustedRandIndex(fused.clusters.df$fused.clusters, fused.clusters.df$Phuket)
+adjustedRandIndex(fused.clusters.df$fused.clusters, fused.clusters.df$Washington)
+
+
+
+library(mclust)
+comparisons <- expand.grid(names(per.omic.clusters), names(per.omic.clusters))
+sapply(seq_len(nrow(comparisons)), function(i) {
+  a <- comparisons[i, 1]; b <- comparisons[i, 2]
+  adjustedRandIndex(per.omic.clusters[[a]], per.omic.clusters[[b]])
+})
+
+sapply(per.omic.clusters, function(cl) adjustedRandIndex(cl, fused.clusters))
+
+
+library(pheatmap)
+
+max.aff <- max(sapply(per.omic.graphs, max))
+breaks.common <- seq(0, max.aff, length.out = 101)
+for (name in names(omics.list)) {
+  aff <- per.omic.graphs[[name]]
+  cl <- per.omic.clusters[[name]]
+  ord <- order(cl)
+  
+  ann <- data.frame(cluster = factor(cl[ord]))
+  rownames(ann) <- rownames(aff)[ord]
+  
+  pheatmap(aff[ord, ord],
+           cluster_rows = FALSE, cluster_cols = FALSE,
+           annotation_row = ann, annotation_col = ann,
+           show_rownames = FALSE, show_colnames = FALSE,
+           main = paste("Affinity graph -", name),
+           breaks = breaks.common,
+          filename = paste0("affinity_graph_", name, ".png"),
+          width = 6, height = 5.5, dpi = 300)
+  
+}
+# order patients by fused cluster assignment
+ord.fused <- order(fused.clusters)
+
+ann.fused <- data.frame(cluster = factor(fused.clusters[ord.fused]))
+rownames(ann.fused) <- rownames(fused.aff)[ord.fused]
+
+pheatmap(fused.aff[ord.fused, ord.fused],
+         cluster_rows = FALSE, cluster_cols = FALSE,
+         annotation_row = ann.fused, annotation_col = ann.fused,
+         show_rownames = FALSE, show_colnames = FALSE,
+         main = "Affinity graph - Fused",
+         breaks = breaks.common,
+         filename = "affinity_graph_Fused.png",
+         width = 6, height = 5.5, dpi = 300)
+
